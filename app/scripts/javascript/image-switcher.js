dc52f1b3810ab6e6e19e13c7d992c4f78385667b
Owen Doyle <owen.doyle@ucdconnect.ie>
2017-05-08 22:57:08 +0100

    Added background image switching

---
+++ app/scripts/javascript/image-switcher.js
@@ -0,0 +1,28 @@
+var bgImageArray = [
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg"
+],
+
+base = "assets/backgrounds/",
+secs = 10;
+bgImageArray.forEach(function(img){
+    new Image().src = base + img; 
+    // caches images, avoiding white flash between background replacements
+});
+
+function backgroundSequence() {
+	window.clearTimeout();
+	var k = 0;
+	for (i = 0; i < bgImageArray.length; i++) {
+		setTimeout(function(){ 
+			document.documentElement.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+			document.documentElement.style.backgroundSize ="cover";
+		if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
+		}, (secs * 1000) * i)	
+	}
+}
+backgroundSequence();
